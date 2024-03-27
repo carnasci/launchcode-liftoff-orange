28d1d84dcff85349549c28287cbd334da214bb36
carnasci <cen.master24@gmail.com>
2024-03-27 11:42:36 -0500

    tydied up browser router app.jsx a bit

--- ReactFrontEnd/src/App.jsx
+++ ReactFrontEnd/src/App.jsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './Components/Header'
-import Slideshow from './Components/Slideshow'
-import Home from './Components/Home';
-import Itinerary from './Components/Itinerary';
-import ParkSearch from './Components/ParkSearch';
-import Favorites from './Components/Favorites';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
+import { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Slideshow from "./Components/Slideshow";
+import Home from "./Components/Home";
+import Itinerary from "./Components/Itinerary";
+import ParkSearch from "./Components/ParkSearch";
+import Favorites from "./Components/Favorites";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 // ### ORIGINAL BEFORE ROUTER ###
 // This page is being used as "home" page
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route index element={<Home/>} />
-      <Route element={<ParkSearch />} />
-      <Route element={<Favorites />} />
-      <Route element={<Itinerary />} />
-    </Routes>
-  </BrowserRouter>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/parkSearch" element={<ParkSearch />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/itineraty" element={<Itinerary />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
