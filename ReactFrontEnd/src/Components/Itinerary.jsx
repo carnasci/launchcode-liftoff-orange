72f24413a86877f144df1e4a83a6634eafc46ab5
carnasci <cen.master24@gmail.com>
2024-04-23 18:05:37 -0500

    fixed reset itinerary object

--- ReactFrontEnd/src/Components/Itinerary.jsx
+++ ReactFrontEnd/src/Components/Itinerary.jsx
@@ -7,120 +7,120 @@ import "react-datepicker/dist/react-datepicker.css";
 import ItineraryServices from '../Services/ItineraryServices'
 
 function Itinerary() {
+  //used for react query testing
+  // const data = queryClient.getQueryData(["someparks"])
+  const singlePark = queryClient.getQueryData(["singlePark"]);
 
-//used for react query testing
-// const data = queryClient.getQueryData(["someparks"])
-const singlePark = queryClient.getQueryData(["singlePark"])
+  const parkDetails = singlePark.data[0];
+  // // const parksFound = queryClient.getQueryData(['parksFound'])
+  // console.log("HERE IS THE REACT QUERY CACHED DATA FROM HOME")
+  // console.log(data)
+  // console.log("HERE IS THE REACT QUERY CACHED DATA FROM PARK DETAILS")
+  // console.log(data)
 
-const parkDetails = singlePark.data[0]
-// // const parksFound = queryClient.getQueryData(['parksFound'])
-// console.log("HERE IS THE REACT QUERY CACHED DATA FROM HOME")
-// console.log(data)
-// console.log("HERE IS THE REACT QUERY CACHED DATA FROM PARK DETAILS")
-// console.log(data)
+  const [startDate, setStartDate] = useState(new Date());
 
-const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-const [endDate, setEndDate] = useState(new Date());
+  const user = {
+    id: 2,
+    username: "paulUser",
+    password: "userPaul",
+  };
 
-const user = {
-  id: 2,
-  username: "paulUser",
-  password: "userPaul",
-};
-
-const [itinerary, setItinerary] = useState({
-  id: "",
-  startDate: new Date(),
-  endDate: new Date(),
-  parkCode: parkDetails.parkCode,
-  user: user,
-});
-
-const navigate = useNavigate();
-
-const handleChange = (name, date) => {
-  setItinerary({ ...itinerary, [name]: date });
-};
-
-const saveItinerary = (e) => {
-  setItinerary({ ...itinerary, user: user });
-  e.preventDefault();
-  ItineraryServices.CreateItinerary(itinerary)
-    .then((response) => {
-      console.log(response);
-      //navigate("/home")
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-const reset = (e) => {
-  e.preventDefault();
-  setItinerary({
+  const [itinerary, setItinerary] = useState({
     id: "",
     startDate: new Date(),
     endDate: new Date(),
-    parkCode: user,
+    parkCode: parkDetails.parkCode,
+    user: user,
   });
-};
 
-return (
-  <>
-    <Header />
+  const navigate = useNavigate();
 
-    <div className="bg-gray-400 pt-10 pb-4 mt-10 mx-10 mb-20">
-      <div>
-        <h1 className="underline underline-offset-4 px-5 py-4 text-amber-950">
-          Create Itinerary for {parkDetails.name}
-        </h1>
-      </div>
-      <div>
-        <p className="font-semibold">Trip Start date:</p>
-        <DatePicker
-          showIcon
-          toggleCalendarOnIconClick
-          selected={itinerary.startDate}
-          selectsStart
-          startDate={startDate}
-          endDate={endDate}
-          onChange={(date) => handleChange("startDate", date)}
-          name="startDate"
-        />
-      </div>
-      <div>
-        <p className="font-semibold">Trip End date:</p>
-        <DatePicker
-          showIcon
-          toggleCalendarOnIconClick
-          selected={itinerary.endDate}
-          selectsEnd
-          startDate={startDate}
-          endDate={endDate}
-          minDate={startDate}
-          onChange={(date) => handleChange("endDate", date)}
-          name="endDate"
-        />
-      </div>
-      <div className="flex justify-center space-x-10 pt-6">
-        <button
-          className=" bg-green-700 hover:bg-green-900 font-semibold text-amber-950"
-          onClick={saveItinerary}
-        >
-          Submit
-        </button>
+  const handleChange = (name, date) => {
+    setItinerary({ ...itinerary, [name]: date });
+  };
 
-        <button
-          className="bg-red-700 hover:bg-red-900 font-semibold text-amber-950"
-          onClick={reset}
-        >
-          Reset
-        </button>
+  const saveItinerary = (e) => {
+    setItinerary({ ...itinerary, user: user });
+    e.preventDefault();
+    ItineraryServices.CreateItinerary(itinerary)
+      .then((response) => {
+        console.log(response);
+        //navigate("/home")
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const reset = (e) => {
+    e.preventDefault();
+    setItinerary({
+      id: "",
+      startDate: new Date(),
+      endDate: new Date(),
+      parkCode: parkDetails.parkCode,
+      user: user,
+    });
+  };
+
+  return (
+    <>
+      <Header />
+
+      <div className="bg-gray-400 pt-10 pb-4 mt-10 mx-10 mb-20">
+        <div>
+          <h1 className="underline underline-offset-4 px-5 py-4 text-amber-950">
+            Create Itinerary for {parkDetails.name}
+          </h1>
+        </div>
+        <div>
+          <p className="font-semibold">Trip Start date:</p>
+          <DatePicker
+            showIcon
+            toggleCalendarOnIconClick
+            selected={itinerary.startDate}
+            selectsStart
+            startDate={startDate}
+            endDate={endDate}
+            onChange={(date) => handleChange("startDate", date)}
+            name="startDate"
+          />
+        </div>
+        <div>
+          <p className="font-semibold">Trip End date:</p>
+          <DatePicker
+            showIcon
+            toggleCalendarOnIconClick
+            selected={itinerary.endDate}
+            selectsEnd
+            startDate={startDate}
+            endDate={endDate}
+            minDate={startDate}
+            onChange={(date) => handleChange("endDate", date)}
+            name="endDate"
+          />
+        </div>
+        <div className="flex justify-center space-x-10 pt-6">
+          <button
+            className=" bg-green-700 hover:bg-green-900 font-semibold text-amber-950"
+            onClick={saveItinerary}
+          >
+            Submit
+          </button>
+
+          <button
+            className="bg-red-700 hover:bg-red-900 font-semibold text-amber-950"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
 }
 
 export default Itinerary
